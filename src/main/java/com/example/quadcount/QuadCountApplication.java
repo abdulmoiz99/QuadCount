5ca8c9d338fc3a21deea7c415dd418d70d9f7e55
Abdul Moiz <moiza0799@gmail.com>
2024-10-21 14:42:21 -0500

    Initial Commit

---
+++ src/main/java/com/example/quadcount/QuadCountApplication.java
@@ -0,0 +1,13 @@
+package com.example.quadcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadCountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quadcount/QuadCountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quadcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadCountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
