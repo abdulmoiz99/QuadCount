2ca94a6dc57ee088441b2de914d5db5325f8cb4f
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2024-10-23 11:41:23 -0500

    authentication

--- src/main/java/com/example/quadcount/controller/JwtAuthenticationController.java
+++ src/main/java/com/example/quadcount/controller/JwtAuthenticationController.java
@@ -11,10 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,7 +28,7 @@ public class JwtAuthenticationController {
                     new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
             User u = (User) authentication.getPrincipal();
             String token = jwtTokenUtil.generateToken(u);
-            return ResponseEntity.ok(new AuthResponse(token, u.getEmail()));
+            return ResponseEntity.ok(new AuthResponse(token, u));
         } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
--- src/main/java/com/example/quadcount/model/AuthResponse.java
+++ src/main/java/com/example/quadcount/model/AuthResponse.java
@@ -7,5 +7,5 @@ import lombok.Data;
 @Data
 public class AuthResponse {
     private String token;
-    private String email;
+    private User user;
 }
