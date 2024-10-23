954f98f2f91dc14db11c15b81c5424a6b13794a9
Abdul Moiz <moiza0799@gmail.com>
2024-10-23 13:18:26 -0500

    Code rebase

--- src/main/java/com/example/quadcount/repository/userRepository.java
+++ src/main/java/com/example/quadcount/repository/userRepository.java
@@ -1,7 +1,12 @@
 package com.example.quadcount.repository;
 
-import com.example.quadcount.model.Users;
+import com.example.quadcount.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface userRepository extends JpaRepository<Users, Integer> {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
